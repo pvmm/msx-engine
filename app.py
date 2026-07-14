801e4fc3bdc9a124cd11c8e0d3fa00ce71513d11
Pedro de Medeiros <pedro.medeiros@gmail.com>
2026-07-14 00:48:59 -0300

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-
 import os
-import base64
 
-from gui.tileviewer import TileViewer
+from tileviewer import TileViewer
 
 
 port = int(os.getenv("PORT", 7860))
 
 
 TileViewer()
-
 
 ui.run(
     host="0.0.0.0",
